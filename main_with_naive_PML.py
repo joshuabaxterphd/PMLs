0afe8d1203d4edc3254205e9ed97b536747e85d9
joshuabaxterphd <joshuabaxterphd@gmail.com>
2024-12-20 19:42:17 -0500

    Update main_with_naive_PML.py

--- main_with_naive_PML.py
+++ main_with_naive_PML.py
@@ -10,16 +10,19 @@ mu0 = 1.256637062e-6 # permeability of free space
 c0 = 2.99792458e8 # speed of light in vacuum
 imp0 = np.sqrt(mu0/eps0) # impedance of free space
 
+# Simulation domain size, step size, etc
 simulation_size = 20e-6
 step_size = 5e-9 # dy
 N_space_cells = int(simulation_size/step_size) # jmax
 print(f"there are {N_space_cells} FDTD cells")
 
+# Simulation time step size, and total simulation time
 dt = step_size/c0
 simulation_time = 1e-12
 N_time_steps = int(simulation_time/dt)
 print(f"there are {N_time_steps} FDTD time steps")
 
+# allocate memory for everything
 Ex = np.zeros(N_space_cells)
 Hz = np.zeros(N_space_cells)
 eps = np.ones(N_space_cells) #* 2
@@ -27,49 +30,37 @@ sigma = np.zeros(N_space_cells)
 sigma_h = np.zeros(N_space_cells)
 refractive_index = np.sqrt(eps)
 
-pml_size = 10
-pml_cond_e = -np.log(1e-3) * c0 * eps0 / (2 * refractive_index[-pml_size] * pml_size * step_size)
+# set up PML for +y side
+pml_size = 10  # number of pml cells 
+pml_cond_e = -np.log(1e-3) * c0 * eps0 / (2 * refractive_index[-pml_size] * pml_size * step_size) # electric conductivity
 
-sigma[-pml_size:] = pml_cond_e
-sigma_h[-pml_size:] = pml_cond_e * mu0 / (eps0 * eps[-pml_size:])
-
-
-# thin_film_thickness = 0e-6
-# j_Si_start = N_space_cells // 2
-# j_Si_end = int(thin_film_thickness/step_size + j_Si_start - 1)
-# eps[j_Si_start:j_Si_end+1] = 12.11
-# eps[j_Si_end+1:] = 4.0
-
-
+sigma[-pml_size:] = pml_cond_e # electric conductivity
+sigma_h[-pml_size:] = pml_cond_e * mu0 / (eps0 * eps[-pml_size:]) # magnetic conductivity
 plt.plot(sigma)
 plt.show()
 
-h_coeff = dt/(mu0 * step_size)
-
+# Electric field update coefficients
 denominator = eps0 * eps/dt + sigma/2
 e_coeff_1 = (eps0 * eps/dt - sigma/2) / denominator
 e_coeff_2 = 1.0 / (step_size * denominator)
 
+# Magnetic field update coefficients
 denominator_h = mu0/dt + sigma_h/2
 h_coeff_1 = (mu0/dt - sigma_h/2) / denominator_h
 h_coeff_2 = 1.0 / (step_size * denominator_h)
 
+# Mur BC coefficients
 c = c0 / refractive_index[0]
 c_ = c0 / refractive_index[-1]
 a = (c * dt - step_size) / (c * dt + step_size)
 a_ = (c_ * dt - step_size) / (c_ * dt + step_size)
 pml_size = 0
 
-# set up pulse stuff
+# set up source stuff
 center_wavelength = 1550e-9
 omega0 = 2 * np.pi * c0 / center_wavelength
 pulse_width = 10e-15
 pulse_delay = 4 * pulse_width
-
-# time = np.linspace(0,simulation_time,N_time_steps)
-# pulse = signal(time,pulse_width,pulse_delay,omega0)
-# plt.plot(time,pulse)
-# plt.show()
 
 j_source = 10 + pml_size
 t_offset = refractive_index[j_source] * step_size / (2 * c0)
@@ -102,6 +93,7 @@ for n in range(N_time_steps):
     # add electric field source
     Ex[j_source] = Ex[j_source] + signal((n + 1)*dt, pulse_width, pulse_delay, omega0)
 
+    # Mur Boundary condition for -y side
     Ex[0] = Ex_prev[1] + a * (Ex[1] - Ex_prev[0])
 
     # store reflection and transmission data
@@ -123,25 +115,17 @@ omegas = 2 * np.pi * c0 /wavelengths
 time = np.arange(N_time_steps) * dt
 
 def Discrete_Fourier_Transform(field, time, omega):
+    # Calculates Discrete fourier transform of field(time) at frequency points in omega
     N_freq = omega.shape[0]
     field_omega = np.zeros(N_freq, dtype= 'complex128')
     for w in range(N_freq):
         field_omega[w] = np.sum(field * np.exp(1j * omega[w] * time))
     return field_omega
 
-def thin_film_TR(n1,n2,n3,wavelengths,thickness):
-    r12 = (n1 - n2)/(n1 + n2)
-    r23 = (n2 - n3)/(n2 + n3)
-    t12 = 2 * n1 / (n1 + n2)
-    t23 = 2 * n2 / (n2 + n3)
-    beta = 2 * np.pi * n2 * thickness / wavelengths
-    r = (r12 + r23 * np.exp(-2 * 1j * beta))/(1.0 + r12 * r23 * np.exp(-2 * 1j * beta))
-    t = (t12 * t23 * np.exp(-1j * beta))/(1.0 + r12 * r23 * np.exp(-2 * 1j * beta))
-    return (n3/n1) * np.abs(t) ** 2, np.abs(r) ** 2
-
-
 pulse = signal(time,pulse_width,pulse_delay,omega0)
 
+
+# Calculate and Plot the transmittance spectra 
 ET_FT = Discrete_Fourier_Transform(ET,time,omegas)
 ER_FT = Discrete_Fourier_Transform(ER,time,omegas)
 pulse_FT = Discrete_Fourier_Transform(pulse,time,omegas)
@@ -149,21 +133,14 @@ pulse_FT = Discrete_Fourier_Transform(pulse,time,omegas)
 R = np.abs(ER_FT) ** 2 / np.abs(pulse_FT) ** 2
 T = np.abs(ET_FT) ** 2 / np.abs(pulse_FT) ** 2 * refractive_index[jT] / refractive_index[j_source]
 
-# T_, R_ = thin_film_TR(1.0,np.sqrt(12.11),np.sqrt(4), wavelengths, thin_film_thickness)
-
 plt.plot(wavelengths, R, 'blue')
-# plt.plot(wavelengths, R_, 'x', color = 'blue')
-# plt.plot(wavelengths, T, 'red')
-# plt.plot(wavelengths, T_, 'x', color = 'red')
-# plt.plot(wavelengths,1-R-T,"violet")
 plt.xlabel("wavelengths (m)")
 plt.ylabel("Spectrum")
-# plt.legend(["Reflectance","R-analytic", "Transmittance","T-analytic","Absorption"])
-plt.legend(["Reflectance", "Transmittance"])
 plt.tight_layout()
 plt.show()
 
 
+# Create an animation of the electric field and play the video
 frames = [] # for storing the generated images
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
